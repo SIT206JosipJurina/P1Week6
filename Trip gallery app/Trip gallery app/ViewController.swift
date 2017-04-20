d89012fc5ed1223bc5869c502d6947aa7094b4e3
JOSIP ANTONY JURINA <jjurina@10-150-44-123.dhcp-m.deakin.edu.au>
2017-04-20 13:29:55 +1000

    1st check in

---
+++ Trip gallery app/Trip gallery app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trip gallery app
+//
+//  Created by JOSIP ANTONY JURINA on 20/04/2017.
+//  Copyright © 2017 JOSIP ANTONY JURINA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
